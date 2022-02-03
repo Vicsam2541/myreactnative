d65a629dded74980d4efb938e3643ae25d895a99
Vicsam2541 <86455749+Vicsam2541@users.noreply.github.com>
2022-02-03 17:22:39 +0700

    week11

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -8,21 +8,49 @@ import Resort from "../screens/week4/Resort";
 import Book from "../screens/week9/Book";
 import BookDetail from "../screens/week9/BookDetail";
 import BookForm from "../screens/week9/BookForm";
+import Location from "../screens/week11/Location";
+import LocationQuiz from "../screens/week11/LocationQuiz";
 
 const Stack = createStackNavigator();
 
 export default function RootStack() {
   return (
     <Stack.Navigator initialRouteName="BottomTab">
-      <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
-      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
-      <Stack.Screen name="Travel" component={Travel} options={{ title : "Travel" }}  />
-      
-      <Stack.Screen name="Health" component={Health} options={{ title : "Health" }}  />
-      <Stack.Screen name="Resort" component={Resort} options={{ title : "Resort" }}  />
+      <Stack.Screen
+        name="BottomTab"
+        component={BottomTab}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PokemonTab"
+        component={PokemonTab}
+        options={{ title: "Pokemon World" }}
+      />
+      <Stack.Screen
+        name="Travel"
+        component={Travel}
+        options={{ title: "Travel" }}
+      />
+
+      <Stack.Screen
+        name="Health"
+        component={Health}
+        options={{ title: "Health" }}
+      />
+      <Stack.Screen
+        name="Resort"
+        component={Resort}
+        options={{ title: "Resort" }}
+      />
       <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
-      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
-      <Stack.Screen name="BookForm" component={BookForm}   />
+      <Stack.Screen
+        name="BookDetail"
+        component={BookDetail}
+        options={{ title: "Book Detail" }}
+      />
+      <Stack.Screen name="BookForm" component={BookForm} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
