8dfc4104ccfae5968ae66eeb66938c8e203d99e3
Vicsam2541 <86455749+Vicsam2541@users.noreply.github.com>
2022-02-17 17:32:32 +0700

    week13

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import React from "react";
 import BottomTab from "./navigations/BottomTab";
 import HomeStack from "./navigations/HomeStack";
 import RootStack from "./navigations/RootStack";
-
-
+import { AuthContext, AuthContextProvider } from "./hooks/AuthContextProvider";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <BottomTab /> */}
-      <RootStack />
-    </NavigationContainer>
+    <AuthContextProvider>
+      <NavigationContainer>
+        {/* <BottomTab /> */}
+        <RootStack />
+      </NavigationContainer>
+    </AuthContextProvider>
   );
 }
